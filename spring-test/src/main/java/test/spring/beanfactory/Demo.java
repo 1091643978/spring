b1f1a5a91c729a5684ede6182f171d826b3a65ca
liubo <1091643978@qq.com>
2019-10-31 13:20:55 +0800

    reader event listener

---
+++ spring-test/src/main/java/test/spring/beanfactory/Demo.java
@@ -0,0 +1,16 @@
+package test.spring.beanfactory;
+
+/**
+ * @author: liubo
+ * @date: 2019-10-24 20:33
+ * @description:
+ **/
+public class Demo {
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("while true");
+            continue;
+        }
+    }
+}
